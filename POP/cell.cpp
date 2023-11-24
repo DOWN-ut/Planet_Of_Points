3965af55928aea5697662b4d1d29690cb3f82d5e
Arthur <arthurvillaroya@gmail.com>
2023-11-24 11:57:20 +0100

    cell

--- POP/cell.cpp
+++ POP/cell.cpp
@@ -1,6 +1,32 @@
 #include "cell.h"
 
 Cell::Cell()
-{
+{      
 
+}
+
+Cell::Cell(int x, int y, int z){
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Cell::Cell(int x, int y, int z, float pressure, float temperature){
+    this->x = x;
+    this->y = y;
+    this->z = z;
+
+    this->pressure = pressure;
+    this->temperature = temperature;
+}
+
+
+int Cell::getX(){
+    return this->x;
+}
+int Cell::getY(){
+    return this->y;
+}
+int Cell::getZ(){
+    return this->z;
 }
--- POP/cell.h
+++ POP/cell.h
@@ -1,11 +1,28 @@
+#include "point.h"
+
 #ifndef CELL_H
 #define CELL_H
 
 
 class Cell
 {
-public:
-    Cell();
+    private:
+        int x;
+        int y;
+        int z;
+
+        Point points[1000];
+        float pressure;
+        float temperature;
+
+    public:
+        Cell();
+        Cell(int x, int y, int z);
+        Cell(int x, int y, int z, float pressure, float temperature);
+
+        int getX();
+        int getY();
+        int getZ();
 };
 
 #endif // CELL_H
