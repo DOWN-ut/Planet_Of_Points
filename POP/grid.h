78d07dcf4ae2949e31695af43fb7efd7068f2c59
Saint-Val Yahnis <yahnis.saint-val@etu.umontpellier.fr>
2023-11-24 10:28:10 +0100

    gitignore

--- POP/grid.h
+++ POP/grid.h
@@ -2,7 +2,7 @@
 #define GRID_H
 
 #include "cell.h"
-
+#include <QVector3D>
 
 class Grid
 {
@@ -16,6 +16,19 @@ private:
 public:
     Grid();
     Grid(float size,int resolution);
+
+    int getId(int x, int y, int z)
+    {
+        return x + (y*resolution) + (z*resolution*resolution);
+    }
+    Cell getCell(int x, int y, int z)
+    {
+        return cells[getId(x,y,z)];
+    }
+    Cell getCell(QVector3D pos)
+    {
+        return getCell((int)pos.x(),(int)pos.y(),(int)pos.z());
+    }
 };
 
 #endif // GRID_H
